d035a665f187ccb22c3339d184cee3fb50a22713
Nélio  DIas <nelio.dias@gotechds.com.br>
2024-09-08 12:20:30 -0300

    feat: create Register views functions, urls and template

--- registers/urls.py
+++ registers/urls.py
@@ -4,4 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
+    path("register/expense", views.register_expense,
+         name="register_expense"),
+    path("register/sucess", views.expense_success, name="expense_success"),
 ]
--- registers/views.py
+++ registers/views.py
@@ -1,5 +1,24 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from .forms import ExpenseForm
 # Create your views here.
+
+
 def index(request):
     return render(request, 'core/index.html')
+
+
+def register_expense(request):
+    if (request.method == 'POST'):
+        form = ExpenseForm(request.POST)
+        if form.is_valid():
+            expense = form.save(commit=False)
+            expense.user = request.user
+            expense.save()
+            return redirect('expense_success')
+    else:
+        form = ExpenseForm()
+    return render(request, 'register/expense_form.html', {'form': form})
+
+
+def expense_success(request):
+    return render(request, 'register/expense_success.html')
