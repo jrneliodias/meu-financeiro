7042ef87386e356d5c4c371df10738c9bf6110f7
Nelio Dias <jrneliodias@gmail.com>
2025-05-15 13:31:43 -0300

    feat: Implement billing period calculation logic and add unit tests

--- reports/admin.py
+++ reports/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
-
+from .models import BillingPeriod
 # Register your models here.
+
+
+class BillingPeriodAdmin(admin.ModelAdmin):
+    list_display = ('start_date', 'end_date', 'billing_year', 'billing_month', 'payment_method'
+                    )
+
+
+admin.site.register(BillingPeriod, BillingPeriodAdmin)
